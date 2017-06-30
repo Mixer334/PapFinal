823a83b362bddbc55abc58dcc37539a6d45c31ec
Pedro <pedroferreira272@hotmail.com>
2017-06-30 17:12:32 +0100

    V6
    hh

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController } from 'ionic-angular';
 import 'rxjs/add/operator/map';
 import {Http} from '@angular/http';
 import {PostPage} from '../../pages/post/post';
+import {LoadingController} from 'ionic-angular';
 
 
 @Component({
@@ -14,17 +15,48 @@ export class HomePage {
 	api_url: string = 'http://aeal.edu.pt/wp-json/wp/v2/posts';
 	items: any;
 
-	constructor( private http: Http,public navCtrl: NavController ) {
+	constructor( private http: Http,public navCtrl: NavController, public loadingCtrl: LoadingController) {
   this.http.get(this.api_url).map(res => res.json()).subscribe(data => {
 
           this.items=data;
         }, (error)=> {
            console.log('error',error);
         });
+        this.presentLoadingCustom()
     }
 
     itemTapped(event, item){
         this.navCtrl.push(PostPage, { item: item });
     }
 
-  }
+
+
+presentLoadingCustom() {
+  let loading = this.loadingCtrl.create({
+    spinner:`hide`,
+    content: `
+    <div class="preloader">
+    <div class="loader"></div>
+    </div>
+    Carregando`,
+    duration: 2000
+  });
+
+  loading.onDidDismiss(() => {
+    console.log('Dismissed loading');
+  });
+
+  loading.present();
+}
+/*presentLoadingDefault() {
+  let loading = this.loadingCtrl.create({
+    content: 'Please wait...'
+  });
+
+  loading.present();
+
+  setTimeout(() => {
+    loading.dismiss();
+  }, 5000);
+}*/
+}
